949201aa26dbb8eb1b62c238e52a27572cf6a10f
EGORDYU <egordyuzhev@gmail.com>
2023-04-06 13:28:50 -0600

    adds server and users route sudocode

---
+++ controllers/users.js
@@ -0,0 +1,18 @@
+//required packages
+const express = require('express')
+const router = express.Router()
+
+//mount routes on router
+
+
+//GET /users/new -- show route for a form that creates a new user
+//(sign up for the app)
+
+
+//POST /users -- CREATE a new user from the form @ GET /users/new
+
+
+
+//export the router instance
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,25 @@
+//require packages
+const express = require('express')
+require('dotenv').config()
+
+
+// app config
+const app = express()
+const PORT = process.env.PORT || 8000
+app.set('view engine', 'ejs')
+
+// middlewares
+//parse html form request obdies
+app.use(express.urlencoded({ extended: false }))
+
+// routes and controllers
+app.get('/', (req, res) =>{
+    res.send('welcome to the auth api');
+})
+
+app.use('/users', require('./controllers/users.js'))
+
+//app.listen
+app.listen(PORT, () => {
+    console.log(`authenticating users on port ${PORT}`)
+})
